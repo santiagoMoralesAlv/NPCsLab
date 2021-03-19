2a146b2307f9bc7c2397243f80e5faf10f1a581e
Santiago Morales <santiago.is.afk@gmail.com>
2021-03-19 10:27:52 -0500

    merge with grevict

---
+++ Assets/Scripts/ObstaclesControll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesControll : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Death")
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
